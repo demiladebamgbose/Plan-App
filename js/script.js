52e644f7bc1fea6ce9da645f87c7c9a36faa3bf9
demiladebamgbose <demiladebamgbose>
2016-04-19 03:49:19 +0100

    a form for new tasks

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 function showLogIn(){
-
+	var button = document.getElementById("btnlogin")
+	button.style.background="#eee"
+	var button_sign_up = document.getElementById("btnsignup")
+	button_sign_up.style.background="#de9193"
 	document.getElementById("login").style.display="block" ;
 	document.getElementById("signup").style.display="none";
 }
 function showSignUp(){
+	var button = document.getElementById("btnsignup")
+	button.style.background="#eee"
+	var button_sign_up = document.getElementById("btnlogin")
+	button_sign_up.style.background="#de9193"
 	document.getElementById("signup").style.display="block" ;
 	document.getElementById("login").style.display="none";
 }
-function showHome(){
-	location.replace("http://www.google.com");
 
-}
+
+var namefield= getElementById('name')
